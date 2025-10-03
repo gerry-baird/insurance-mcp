d160d9d75f283db3551d878651a09b45dae500cf
Gerry Baird <gerry.baird@uk.ibm.com>
2025-10-03 11:06:22 +0100

    os

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from typing import List
 from datetime import date
 from model import Customer, Policy
 from data import get_all_customers, get_customer_by_id, get_customers_by_state, get_all_policies, get_policy_by_id, get_policies_by_customer_id, init_database, init_sample_data, ensure_fresh_sample_data
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+PORT = os.environ.get("PORT", 8000)
 
 # Create an MCP server
-mcp = FastMCP("insurance-mcp", host="127.0.0.1", port=8000)
+mcp = FastMCP("insurance-mcp", host="127.0.0.1", port=PORT)
 
 @mcp.tool()
 async def get_customers():
